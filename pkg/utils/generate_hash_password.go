a114975dda991124bb27fb424abac4c31b913378
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-01-11 20:45:52 +0700

    feat: add response to client if error in generate hash password function

--- pkg/utils/generate_hash_password.go
+++ pkg/utils/generate_hash_password.go
@@ -9,6 +9,7 @@ import (
 func GenerateHashPassword(w http.ResponseWriter, password string) ([]byte, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return nil, err
 	}
 
