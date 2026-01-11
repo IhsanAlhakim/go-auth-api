ef1bc96bdc2d536edbffad8f1d2df0bbb78a8120
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-01-11 21:47:28 +0700

    refactor: remove unused switch

--- pkg/handlers/request.go
+++ pkg/handlers/request.go
@@ -7,22 +7,13 @@ import (
 )
 
 func DecodeRequestBody(w http.ResponseWriter, r *http.Request, payload any) error {
-	err := json.NewDecoder(r.Body).Decode(payload)
-
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(payload); err != nil {
 		if err == io.EOF {
 			http.Error(w, "Request body must not be empty", http.StatusBadRequest)
 		} else {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		return err
-	}
-
-	switch {
-	case err == io.EOF:
-
-	case err != nil:
-
 	}
 	return nil
 }
